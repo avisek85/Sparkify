f9d2a5b712a4abdac1ce166f04f54bcffbbe7c7d
avisek85 <Abhisheksinghrajput5888@gmail.com>
2024-11-16 02:37:45 +0530

    changes some code

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -6,9 +6,11 @@ const SocketContext = createContext();
 export const useSocket = ()=>useContext(SocketContext);
 
 export const SocketProvider = ({children})=>{
+    const API_URI  = import.meta.env.VITE_API_URL;
+
     const [socket , setSocket] = useState(null);
     useEffect(()=>{
-        const socketConnection = io("http://localhost:3000");
+        const socketConnection = io(`${API_URI}`);
         setSocket(socketConnection);
 
         return ()=>socketConnection.disconnect();
