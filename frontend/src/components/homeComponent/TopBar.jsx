2ee6684072e04ba0c83189e8db7685997e4b694d
avisek85 <Abhisheksinghrajput5888@gmail.com>
2024-11-17 02:08:48 +0530

    changes in chat layout

--- frontend/src/components/homeComponent/TopBar.jsx
+++ frontend/src/components/homeComponent/TopBar.jsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
-import styles from './TopBar.module.css';
-import axios from 'axios';
-import { useAuthContext } from '../../context/AuthContextProvider';
+import React, { useState } from "react";
+import styles from "./TopBar.module.css";
+import axios from "axios";
+import { useAuthContext } from "../../context/AuthContextProvider";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
+function TopBar({ setUsers }) {
+  const { findProfile, getToken } = useAuthContext();
+  const navigate = useNavigate();
+  let userId;
+  if (getToken()) {
+    userId = jwtDecode(getToken());
+  } else {
+    navigate("/login");
+  }
 
-
-function TopBar({setUsers}) {
-
-  const {findProfile} = useAuthContext();
-
- async function handleSearch (e){
- 
-
+  async function handleSearch(e) {
     const data = await findProfile(e.target.value);
     setUsers(data);
     console.log(data);
   }
 
-  const [searchKeyword , setSearchKeyword] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState("");
   return (
     <div className={styles.topBar}>
-      <input type="text" placeholder="🔍 Search..." onChange={(e)=>{
-       handleSearch(e);
-      }}  className={styles.searchBar} />
+      <input
+        type="text"
+        placeholder="🔍 Search..."
+        onChange={(e) => {
+          handleSearch(e);
+        }}
+        className={styles.searchBar}
+      />
       <div className={styles.profileIcon}>
-        <img src="https://www.w3schools.com/w3images/avatar2.png" alt="User" />
+        <img src="https://www.w3schools.com/w3images/avatar2.png" alt="User" onClick={()=>{navigate(`/profile/${userId.id}`)}} />
       </div>
     </div>
   );
